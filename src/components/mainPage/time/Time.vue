38b2729f9521936253575a73e9678768d8a6f44a
shaked007 <shakedbukai@gmail.com>
2022-11-09 00:04:52 +0200

    added-nightsky

--- src/components/mainPage/time/Time.vue
+++ src/components/mainPage/time/Time.vue
@@ -1,23 +1,64 @@
 <template>
-  <div id="tsparticles" class="night-and-day"> 
+  <!-- <div id="tsparticles" class="night-and-day"> 
     <i class="bi bi-moon-fill"></i>
 
+    </div> -->
+    <div class="night-and-day">
+          <i class="bi bi-moon-fill night-icon"></i>
+          <div class="particles-container">
+    <Particles id="tsparticles" :options="options" :particlesInit="particlesInit" />
+    </div>
     </div>
 </template>
 
 <script>
+// import {Engine} from "tsparticles-engine"
+import {loadStarsPreset} from "tsparticles-preset-stars"
+
 export default {
     name:"Time",
+    data(){
+      return{
+        options:{
+           background: {
+    color: {
+      value: "#063c5c",
+    },},
+          fullScreen:{
+            enable:false,
+          },
+          preset:"stars"
+        }
+      }
+    },
     mounted(){
+      
     },
     components:{
         
+    },
+    methods:{
+      async particlesInit(engine){
+        await loadStarsPreset(engine)
+
+      }
     }
 }
 </script>
 
-<style scoped>
-i{
+<style >
+.particles-container{
+  height: 100%;
+ 
+
+}
+.particles-container div{
+  height: 100%;
+}
+ canvas{
+  border-radius: 20px !important;
+}
+.night-icon{
     color: white;
     position: absolute;
     left: 10%;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Particles from "vue3-particles"
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
 
-createApp(App).mount('#app')
+createApp(App).use(Particles).mount('#app')
